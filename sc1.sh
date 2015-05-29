dee55b27f5ae0478128b64eb91716ef899c8f314
kislay17 <srivastav.kislay@gmail.com>
2015-05-29 11:14:41 +0000

    Modified

--- sc1.sh
+++ sc1.sh
@@ -2,7 +2,7 @@
 #Create an empty file
 read -p "Enter the file path :" Path #reads the path of the file
 read -p "Enter the file name :" Fname #reads the file name to be created
-if test "$Fname" = "$path"/*; then #if check condition & greo gives 1 if file exsist at location and 0 if it doesnot 
+if test "$Fname" = "$path"/*; then #if check condition ,it gives 1 if file exsist at location and 0 if it doesnot 
 	echo "File already exsist" #prints the statement if file exsist
 else
 	touch $Path/$Fname #create new file at location if doesnot exsist

bc8503497209b562734304c403273692e42476ce
kumar kislay <srivastav.kislay@gmail.com>
2015-05-31 16:04:42 +0000

    Modified

--- Delete_File_Linenum.sh
+++ Delete_File_Linenum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Deletes a line number from a file
 read -p "Enter the Dir path :" Dpath #reads the file path
-if [ -d "$Dpath"];then #if checks if directory is present or not
+if [ -d "$Dpath" ];then #if checks if directory is present or not
 #if yes
 	read -p "Enter the file name :" Fname #reads file name
 	if test -f $Dpath/$Fname; then #if checks if file is present or not

14fefc8bb4f991d27b879fac59c8885ea54d9868
kumar kislay <srivastav.kislay@gmail.com>
2015-06-01 09:48:06 +0000

    Modified

--- Synchronisation.sh
+++ Synchronisation.sh
@@ -3,17 +3,23 @@
 read -p "Enter the source folder :" Sfldr #reads source folder destination
 if [ -d "$Sfldr" ];then #checks if source folder is present or not
 #if yes
-	read -p "Enter destination folder :" Dfldr #reads Destination folder path
-	if [ -d "$Dfldr" ];then #checks if destination folder is present or not
-	#if yes 
-		rsync -r $Sfldr/ $Dfldr/ #rsync synchronise , r- will allow to sync sub directories 
-		echo "Synchronisation done" #prints statement
+	if [ "$(ls -A $Sfldr)" ];then #checks wether directory is empty or not
+	#if yes
+		read -p "Enter destination folder :" Dfldr #reads Destination folder path
+		if [ -d "$Dfldr" ];then #checks if destination folder is present or not
+		#if yes 
+			rsync -r $Sfldr/ $Dfldr/ #rsync synchronise , r- will allow to sync sub directories 
+			echo "Synchronisation done" #prints statement
+		else
+		#if no
+			mkdir $Dfldr #make destination directory
+			echo "Destination Directory made"
+			rsync -r $Sfldr/ $Dfldr/
+			echo "Synchronisation done"
+		fi
 	else
 	#if no
-		mkdir $Dfldr #make destination directory
-		echo "Destination Directory made"
-		rsync -r $Sfldr/ $Dfldr/
-		echo "Synchronisation done"
+		echo "Source Directory is empty "
 	fi
 else
 #if no
